ccd2794d4c8e68fc036ab4cda54f11cca0eceefc
Ritesh-Aggarwal <57039447+Ritesh-Aggarwal@users.noreply.github.com>
2021-06-17 18:42:55 +0530

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.6f75d03df357489d76d5383261cc61e6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d488ac654ad0470e4d99b1e9a0305594",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "216bfa71dd4cda5eaf68",
+    "url": "/Keeper-App/static/js/2.1a41f7c2.chunk.js"
+  },
+  {
+    "revision": "d4bf941190cd5036ceee",
+    "url": "/Keeper-App/static/js/main.8121fb45.chunk.js"
+  },
+  {
+    "revision": "8c85bae05d866358124a",
+    "url": "/Keeper-App/static/js/runtime-main.fca5ae5c.js"
+  }
+]);
